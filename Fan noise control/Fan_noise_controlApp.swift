880015d954bf6572a1f788ecbece6d7f1c43e7a6
vietanh <vnguyen78@gmail.com>
2026-02-02 14:56:40 +0700

    Initial Commit

---
+++ Fan noise control/Fan_noise_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fan_noise_controlApp.swift
+//  Fan noise control
+//
+//  Created by admin on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Fan_noise_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fan noise controlTests/Fan_noise_controlTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fan_noise_controlTests.swift
+//  Fan noise controlTests
+//
+//  Created by admin on 2/2/26.
+//
+
+import Testing
+@testable import Fan_noise_control
+
+struct Fan_noise_controlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
